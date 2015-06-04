3798b8f94a2b42b93bead07d51439f89817e42a6
Eduard <es1989@gmail.com>
2015-06-04 13:45:29 +0200

    Edit ProjectTO

--- ProjectSystem/ejbModule/de/project/dto/ProjectTO.java
+++ ProjectSystem/ejbModule/de/project/dto/ProjectTO.java
@@ -1,5 +1,67 @@
 package de.project.dto;
 
-public class ProjectTO {
+import java.util.Calendar;
+import java.util.List;
 
+import de.project.entities.*;
+import de.project.enumerations.*;
+
+
+public class ProjectTO {		
+	private static final long serialVersionUID = 1L;
+		private long id;
+		private ProjectStatus projectStatus;
+		private List<User> members;
+		private List<Milestone> milestone;
+		private Calendar updatedOn;
+	
+		public String toString() {
+			return "Project [id=" + id + ", projectStatus=" + projectStatus
+					+ ", member=" + members + "]";
+		}
+
+		public long getId() {
+			return id;
+		}
+
+		public void setId(long id) {
+			this.id = id;
+		}
+
+		public ProjectStatus getProjectStatus() {
+			return projectStatus;
+		}
+
+		public void setProjectStatus(ProjectStatus projectStatus) {
+			this.projectStatus = projectStatus;
+		}
+
+		public List<User> getMembers() {
+			return members;
+		}
+
+		public void setMembers(List<User> members) {
+			this.members = members;
+		}
+
+		public List<Milestone> getMilestone() {
+			return milestone;
+		}
+
+		public void setMilestone(List<Milestone> milestone) {
+			this.milestone = milestone;
+		}
+
+		public Calendar getUpdatedOn() {
+			return updatedOn;
+		}
+
+		public void setUpdatedOn(Calendar updatedOn) {
+			this.updatedOn = updatedOn;
+		}
+		
+		
+		
 }
+	
+	
