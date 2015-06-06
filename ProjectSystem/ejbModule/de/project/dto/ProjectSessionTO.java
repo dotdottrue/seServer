06c4a3a874a0710b46e7c94be0e810d48db48b8a
Eduard <es1989@gmail.com>
2015-06-06 11:25:03 +0200

    added ProjectSessionTO

--- ProjectSystem/ejbModule/de/project/dto/ProjectSessionTO.java
+++ ProjectSystem/ejbModule/de/project/dto/ProjectSessionTO.java
@@ -1,5 +1,36 @@
 package de.project.dto;
 
-public class ProjectSessionTO {
+import java.io.Serializable;
+import java.util.Calendar;
 
+import de.project.entities.User;
+
+
+public class ProjectSessionTO implements Serializable{
+
+	
+	private static final long serialVersionUID = 1L;	
+	private int id;
+	private User user;
+	private Calendar createdAt;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public Calendar getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Calendar createdAt) {
+		this.createdAt = createdAt;
+	}
+	
 }
