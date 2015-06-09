675b3109cc444d8b523e05ce534a352067a16fdf
tobias kappert <tobias.kappert@web.de>
2015-06-09 12:38:45 +0200

    edited dao files

--- ProjectPersistence/ejbModule/de/project/dao/ProjectUserDAO.java
+++ ProjectPersistence/ejbModule/de/project/dao/ProjectUserDAO.java
@@ -1,14 +1,10 @@
 package de.project.dao;
 
 import java.util.Date;
-import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 
 import de.project.dao.local.ProjectUserDAOLocal;
 import de.project.entities.ProjectSession;
