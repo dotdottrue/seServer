d48a9890414e1da8ac7ec650830c9ccc25d35c08
tobias kappert <tobias.kappert@web.de>
2015-06-15 23:17:25 +0200

    write first test user registration

---
+++ ProjectTestClient/src/de/project/test/UserIntegrationTest.java
@@ -0,0 +1,30 @@
+package de.project.test;
+
+import org.junit.*;
+
+import de.project.enumerations.ReturnCode;
+import de.project.integration.ProjectUserResponse;
+import de.project.integration.UserIntegration;
+import de.project.integration.UserIntegrationService;
+
+public class UserIntegrationTest {
+	
+	private static UserIntegration remote;
+	
+	@BeforeClass
+	public static void  initTest() {
+		UserIntegrationService service = new UserIntegrationService();
+		remote = service.getUserIntegrationPort();
+	}
+	
+	/*
+	 * Dieser Test prüft ob ein Benutzer registriert werden kann. 
+	 * Der ReturnCode sollte OK lauten und der Benutzer sollte dem gewünschten entsprechen.
+	 */	
+	@Test
+	public void userRegistrationTest() {
+		ProjectUserResponse userResponse = remote.registerUser("015144530578");
+		Assert.assertEquals(userResponse.getReturnCode(), ReturnCode.OK);
+		Assert.assertEquals(userResponse.getSession().getUser().getPhoneNumber(), "015144530578");
+	}
+}
