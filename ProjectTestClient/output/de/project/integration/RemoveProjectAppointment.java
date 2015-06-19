12e9ba2c44e8adcaea2e50e22828850a5bba219f
tobias kappert <tobias.kappert@web.de>
2015-06-20 00:05:14 +0200

    added integrations

---
+++ ProjectTestClient/output/de/project/integration/RemoveProjectAppointment.java
@@ -0,0 +1,71 @@
+
+package de.project.integration;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java-Klasse für removeProjectAppointment complex type.
+ * 
+ * <p>Das folgende Schemafragment gibt den erwarteten Content an, der in dieser Klasse enthalten ist.
+ * 
+ * <pre>
+ * &lt;complexType name="removeProjectAppointment">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="arg0" type="{http://www.w3.org/2001/XMLSchema}long"/>
+ *         &lt;element name="arg1" type="{http://www.w3.org/2001/XMLSchema}long"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "removeProjectAppointment", propOrder = {
+    "arg0",
+    "arg1"
+})
+public class RemoveProjectAppointment {
+
+    protected long arg0;
+    protected long arg1;
+
+    /**
+     * Ruft den Wert der arg0-Eigenschaft ab.
+     * 
+     */
+    public long getArg0() {
+        return arg0;
+    }
+
+    /**
+     * Legt den Wert der arg0-Eigenschaft fest.
+     * 
+     */
+    public void setArg0(long value) {
+        this.arg0 = value;
+    }
+
+    /**
+     * Ruft den Wert der arg1-Eigenschaft ab.
+     * 
+     */
+    public long getArg1() {
+        return arg1;
+    }
+
+    /**
+     * Legt den Wert der arg1-Eigenschaft fest.
+     * 
+     */
+    public void setArg1(long value) {
+        this.arg1 = value;
+    }
+
+}
